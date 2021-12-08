db6dc9b96c6024c38f9729fe7eed7ec3f9e03108
thanhmaiismycrush <95614936+thanhmaiismycrush@users.noreply.github.com>
2021-12-08 11:14:41 +0700

    Add files via upload

---
+++ bai95.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   //freopen("inp.txt","r",stdin);
+   //freopen("out.txt","w",stdout);
+   int canteen,a[21],gia_menu,mon_meuu[5],ban_chon,duoc_chon[21];
+   cin>>canteen;
+   for (int i=1;i<=canteen;i++)
+   {
+       cin>>a[i];
+   }
+}
