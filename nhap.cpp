de6dd109371dc72c7f64b69f2d76ec22548c8f34
thanhmaiismycrush <95614936+thanhmaiismycrush@users.noreply.github.com>
2021-12-11 06:14:17 +0700

    Update nhap.cpp

--- nhap.cpp
+++ nhap.cpp
@@ -1,18 +1,21 @@
-#include <iostream>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    map<string, int> A; // Khởi tạo một map A
-
-    // Thêm vào map A một số phần tử.
-    A["One"] = 1;
-    A["Two"] = 2;
-    A["Three"] = 3;
-
-    // Duyệt và xuất ra từng cặp giá trị key: value của mỗi phần tử trong map A
-    map<string, int>::iterator it;
-    for (it = A.begin(); it != A.end(); it++) {
-        cout << it->first << ": " << it->second << endl;
+int main() 
+{
+    int n;
+    string a[101],b[101];
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cin>>a[i];
+        //b[i]=a[i].size();
     }
+    sort(a+1,a+n+1,greater< a[101].size() >());
+    cout<<"___"<<endl;
+    for (int i=1;i<=n;i++)
+    {
+        cout<<a[i]<<endl;
+    }
+    return 0;
 }
