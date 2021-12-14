5168a742328c061ba404ad69cf1a1038d42d3031
thanhmaiismycrush <95614936+thanhmaiismycrush@users.noreply.github.com>
2021-12-15 06:23:45 +0700

    Add files via upload

--- bai93.cpp
+++ bai93.cpp
@@ -49,13 +49,11 @@ int main()
    {   
        if (b[i].size()==6)
        {
-           cout<<i;
            if (o2>=10 || v2>=10|| n2>=10){cout<<"DA"<<endl;continue;}
             else {cout<<"NE"<<endl;continue;}
        }
        if (b[i].size()==5)
        {
-           cout<<i;
            if (o>o1[i] && v>v1[i] && n>n1[i]){cout<<"DA"<<endl;continue;}
            else
            {
@@ -65,11 +63,26 @@ int main()
                   c=o1[i]-o;d=v1[i]-v;e=n1[i]-n;
                   for(int i=0;i<nsoccer;i++)
                   {
-                      if (p[i]=="OV" || p[i]=="VO"){if }
+                      if (p[i]=="OV" || p[i]=="VO")
+                      {if (c-o2+o<d-v2+v)
+                      {o2--;o1[i]--;v2--;}else{v2--,v1[i]--,o2--;}}
+                      if (p[i]=="ON" || p[i]=="NO")
+                      {if (c-o2+o<e-n2+n)
+                      {o2--;o1[i]--;n2--;}else{n2--,n1[i]--,o2--;}}
+                      if (p[i]=="NV" || p[i]=="VN")
+                      {if (e-n2+n<d-v2+v)
+                      {n2--;n1[i]--;v2--;}else{v2--,v1[i]--,n2--;}}
+                      if (p[i]=="OVN"||p[i]=="ONV"||p[i]=="NOV"||p[i]=="NVO"||p[i]=="VON"||p[i]=="VNO")
+                      {if (c-o2+o<d-v2+v&&d-v2+v<=e-n2+n){o2--;o1[i]--;v2--;}
+                       if (c-o2+o>=d-v2+v&&c-o2+o<e-n2+n){v2--,v1[i]--,o2--;}
+                       if (e-n2+n<d-v2+v&&c-o2+o>=d-v2+v){n2--;n1[i]--;v2--;}}
                   }
+                  cout<<o1[i]<<v1[i]<<n1[i]<<endl;
+                  if (o1[i]==0&&v1[i]==0&&n1[i]==0){cout<<"DA"<<endl;}
+                  else{cout<<"NE"<<endl;}
                }
            }
        }
    }
-
 }
+//thu lai truong hop 5 3 2 o cout da...
