58a60469ddaff814ed00007b769c0fbd03b142a4
thanhmaiismycrush <95614936+thanhmaiismycrush@users.noreply.github.com>
2021-12-14 23:24:49 +0700

    Add files via upload

--- bai93.cpp
+++ bai93.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include<C:\MinGW\include\c++\9.2.0\x86_64-w64-mingw32\bits\stdc++.h>
 using namespace std;
 int stN(char x)
 {
@@ -8,54 +8,67 @@ int stN(char x)
     tg>>N;
     return N;
 }
-bool comp(string a,string b)
-{
-    if (a.size()!=b.size()){return a.size()<b.size();}
-    else {return a<b;}
-}
 int main()
 {
-   int so_doi_hinh,so_cau_thu;
-   int o2=0,v2=0,n2=0;
-   int a,o1[11],v1[11],n1[11];
-   string doi_hinh,vi_tri_choi,b[11],c[25];
-   cin>>so_doi_hinh;
-   for (int i=1;i<=so_doi_hinh;i++)
+   int nform,nsoccer;
+   int o2=0,v2=0,n2=0,o=0,v=0,n=0,c,d,e;
+   int o1[11],v1[11],n1[11],m[11];
+   string form,b[11],pos;
+   string p[25];
+   cin>>nform;
+   for (int i=1;i<=nform;i++)
    {
-       cin>>doi_hinh;
-       b[i]=doi_hinh;
-       if (doi_hinh.size()==6){a=10;}
-       if (doi_hinh.size()==5)
+       cin>>form;
+       b[i]=form;
+       if (form.size()==5)
        {
-           for (int j=0;j<doi_hinh.size();j++)
-            {if (doi_hinh[j] != '-' && j==0){o1[i]=stN(doi_hinh[j]);}
-            if (doi_hinh[j]!='-'&& j==2){ v1[i]=stN(doi_hinh[j]);}
-            if (doi_hinh[j]!='-' && j==4){ n1[i]=stN(doi_hinh[j]);}}
+           for (int j=0;j<form.size();j++)
+            {
+                if (form[j] != '-' && j==0){o1[i]=stN(form[j]);}
+                if (form[j]!='-'&& j==2){ v1[i]=stN(form[j]);}
+                if (form[j]!='-' && j==4){ n1[i]=stN(form[j]);}
+                
+           }
+           m[i]=min(min(o1[i],v1[i]),min(v1[i],n1[i]));
        }
    }
-   cin>>so_cau_thu;
-   for (int i=1;i<=so_cau_thu;i++)
+   cin>>nsoccer;
+   for (int i=1;i<=nsoccer;i++)
    {
-        cin>>vi_tri_choi;
-        c[i]=vi_tri_choi;
-        for (int j=0;j<vi_tri_choi.size();j++)
-        {
-            if (vi_tri_choi[j] == 'O'){o2++;}
-            if (vi_tri_choi[j]=='V'){v2++;}
-            if (vi_tri_choi[j]=='N'){n2++;}
-        }
+        cin>>pos;  
+        p[i]=pos;
+        for (int j=0;j<pos.size();j++){ 
+        if (pos[j] =='O'){o2++;}//cout<<o2<<endl;}
+        if (pos[j]=='O' && pos[j]!='V' && pos[j]!='N'){o++;}
+        if (pos[j] =='V'){v2++;}
+        if (pos[j]=='V' && pos[j]!='O' && pos[j]!='N'){v++;}
+        if (pos[j] =='N'){n2++;}
+        if (pos[j]=='N' && pos[j]!='V' && pos[j]!='O'){n++;}}
    }
-   sort (c+1,c+1+n,comp())
-   for (int i=1;i<=so_doi_hinh;i++)
-   {
+   for (int i=1;i<=nform;i++)
+   {   
        if (b[i].size()==6)
        {
-           if (o2>=a || v2>=a || n2>=a){cout<<"DA"<<endl;}
-            else {cout<<"NE"<<endl;}
+           cout<<i;
+           if (o2>=10 || v2>=10|| n2>=10){cout<<"DA"<<endl;continue;}
+            else {cout<<"NE"<<endl;continue;}
        }
        if (b[i].size()==5)
        {
-
+           cout<<i;
+           if (o>o1[i] && v>v1[i] && n>n1[i]){cout<<"DA"<<endl;continue;}
+           else
+           {
+               if (o2<=o1[i] || v2<=v1[i] || n2<=n1[i]){cout<<"NE"<<endl;continue;}
+               else 
+               {
+                  c=o1[i]-o;d=v1[i]-v;e=n1[i]-n;
+                  for(int i=0;i<nsoccer;i++)
+                  {
+                      if (p[i]=="OV" || p[i]=="VO"){if }
+                  }
+               }
+           }
        }
    }
 
