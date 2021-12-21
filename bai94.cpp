86180d9fca878cc6d6e5f555268d6495853848fd
thanhmaiismycrush <95614936+thanhmaiismycrush@users.noreply.github.com>
2021-12-21 13:13:26 +0700

    Add files via upload

---
+++ bai94.cpp
@@ -0,0 +1,12 @@
+#include <C:\MinGW\include\c++\9.2.0\x86_64-w64-mingw32\bits\stdc++.h>
+using namespace std;
+int main()
+{
+    int m,n;
+    string a[3001][3001];
+    cin>>m>>n;
+    for (int i=1;i<=m;i++)
+    {for (int j=1;j<=n;j++)
+        {cin>>a[i][j];}}
+    
+}
